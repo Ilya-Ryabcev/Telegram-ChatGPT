18e39e7f2a010cadb6422b9d8755c8ea43f4caf7
Илья <bull.81@list.ru>
2025-04-26 23:44:32 +0300

    сохраняй copy

--- ChatGPTbot.py
+++ ChatGPTbot.py
@@ -1,1 +1,12 @@
-import aiogram
+from aiogram import Bot, Dispatcher
+import asyncio
+
+bot = Bot(token='7743295312:AAFmkYsQYr-5F_w4yaLBqZuI7btK6gmCaiY')
+dp = Dispatcher()
+
+
+async def start_bot():
+    dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(start_bot())
