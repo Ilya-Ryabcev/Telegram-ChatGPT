070ac4f95f2425d1bb87e753c1a6d9b0ce4bd411
Илья <bull.81@list.ru>
2025-04-27 10:34:28 +0300

    сохраняй main

--- ChatGPTbot.py
+++ ChatGPTbot.py
@@ -4,9 +4,12 @@ import asyncio
 bot = Bot(token='7743295312:AAFmkYsQYr-5F_w4yaLBqZuI7btK6gmCaiY')
 dp = Dispatcher()
 
+@dp.message
+async def all_messages(message):
+    print(message.from_user.full_name)
 
 async def start_bot():
-    dp.start_polling(bot)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(start_bot())
